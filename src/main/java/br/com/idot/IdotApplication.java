9f6a4e4dc5241dc7d81f8db8a9248acf1269b16b
Diego de Mello <diegodemello027@gmail.com>
2025-03-10 17:24:22 -0300

    first commit

---
+++ src/main/java/br/com/idot/IdotApplication.java
@@ -0,0 +1,13 @@
+package br.com.idot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/idot/IdotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.idot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
